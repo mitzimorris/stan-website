2432a250549fe5fad3cd0c13621f9a87a065675f
Mitzi Morris <mitzi@panix.com>
2024-05-18 22:14:53 -0400

    fix install table enable/disable logic

--- installation/installer_2.js
+++ installation/installer_2.js
@@ -1,4 +1,4 @@
-var supportedOperatingSystems = ['linux', 'macos', 'windows', 'colab'];
+var supportedOperatingSystems = ['linux', 'macos', 'windows'];
 var supportedInterfaces = ['stan.jl', 'cmdstanpy', 'cmdstanr', 'cmdstan', 'pystan', 'rstan'];
 var supportedInstallers = ['julia-pkg', 'conda', 'cran', 'github-bin', 'pip', 'runiverse', 'github-src'];
 
@@ -15,12 +15,18 @@ var installerOptions = document.querySelectorAll('.installer');
 osOptions.forEach(option => {
     option.addEventListener('click', function() {
         selectedOption(osOptions, this, 'os');
+	resetOptions(interfaceOptions);
+        resetOptions(installerOptions);
+        updateInterfaceOptions();
+        updateInstallerOptions();
     });
 });
 
 interfaceOptions.forEach(option => {
     option.addEventListener('click', function() {
         selectedOption(interfaceOptions, this, 'interface');
+	resetOptions(installerOptions);
+        updateInstallerOptions();
     });
 });
 
@@ -52,12 +58,6 @@ function selectedOption(options, selection, category) {
 function updateCommandMessage() {
     var key = opts.os + "," + opts.interface + "," + opts.installer;
     var commands = {
-        "colab,cmdstan,github-src": "install from github release src.tgz",
-        "colab,cmdstan,github-bin": "install from github release binary.tgz",
-        "colab,cmdstanr,cran": "install.packages('cmdstanr', repos = c('https://mc-stan.org/r-packages/', getOption('repos')))",
-        "colab,cmdstanpy,pip": "pip install cmdstanpy",
-        "colab,pystan,pip": "pip install pystan",
-        "colab,rstan,cran": "install.packages('rstan')",
         "linux,stan.jl,julia-pkg": "using Pkg; Pkg.add('Stan')",
         "linux,cmdstan,github-src": "install from github release src.tgz",
         "linux,cmdstan,github-bin": "install from github release binary.tgz",
@@ -84,10 +84,50 @@ function updateCommandMessage() {
     document.getElementById('command').innerText = command ? command : "Instructions not available.";
 }
 
+function updateInterfaceOptions() {
+    interfaceOptions.forEach(option => {
+        option.classList.remove('disabled');
+        if (opts.os === 'windows' && option.id === 'pystan') {
+            option.classList.add('disabled');
+        }
+    });
+}
+
+function updateInstallerOptions() {
+    installerOptions.forEach(option => {
+        option.classList.remove('disabled');
+        if ((opts.interface === 'cmdstanr'  || opts.interface === 'rstan') && (option.id !== 'cran' && option.id !== 'runiverse')) {
+            option.classList.add('disabled');
+        }
+	if (opts.interface === 'pystan' && option.id !== 'pip') {
+            option.classList.add('disabled');
+        }
+	if (opts.interface === 'cmdstanpy' && (option.id !== 'pip' && option.id !== 'conda')) {
+            option.classList.add('disabled');
+        }
+        if (opts.interface === 'cmdstan' && (option.id !== 'github-src' && option.id !== 'github-bin' && option.id !== 'conda')) {
+            option.classList.add('disabled');
+        }
+	if (opts.interface === 'stan.jl' && option.id !== 'julia-pkg') {
+            option.classList.add('disabled');
+        }
+    });
+}
+
+function resetOptions(options) {
+    options.forEach(option => {
+        option.classList.remove('disabled');
+    });
+}
+
 // Pre-select user's operating system
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById(opts.os).classList.add('selected');
     document.getElementById(opts.interface).classList.add('selected');
     document.getElementById(opts.installer).classList.add('selected');
     updateCommandMessage();
+    resetOptions(interfaceOptions);
+    resetOptions(installerOptions);
+    updateInterfaceOptions();
+    updateInstallerOptions();
 });
